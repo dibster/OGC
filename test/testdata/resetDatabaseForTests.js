6a7b94b21b2129bcf9bd415a8bbd1165cc9081f3
Dave Ashenhurst <dave.ashenhurst@gmail.com>
2014-02-17 10:26:11 +0000

    API done for administration lookups, object and field types

--- test/testdata/resetDatabaseForTests.js
+++ test/testdata/resetDatabaseForTests.js
@@ -1,24 +1,22 @@
-var mongo = require('mongodb');
+var db = require('../../lib/config/env/db');
 
-var Server = mongo.Server,
-    Db = mongo.Db,
-    BSON = mongo.BSONPure;
+console.log('Open Database');
 
-var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('blueprintdb', server);
+//db.open(function(err, db) {
+//    if (!err) {
+//        console.log("Connected to blueprint database");
+//        db.collection('kaboodleobjects', {strict: true}, function(err, collection) {
+//            if (err) {
+//                console.log("The objects collection is empty...");
+//            }
+//        });
+//    }
+//    else {
+//        console.log('err ' + err);
+//    }
+//});
 
-db.open(function(err, db) {
-    if (!err) {
-        console.log("Connected to blueprint database");
-        db.collection('kaboodleobjects', {strict: true}, function(err, collection) {
-            if (err) {
-                console.log("The objects collection is empty...");
-            }
-        });
-    }
-});
-
-exports.emptyDB = function(req,res) {
+var emptyDb = function() {
     console.log("Connected to 'blueprintdb' database");
     db.collection('kaboodleobjects').remove({},function(err,numberRemoved){
         console.log("remove kaboodle objects");
@@ -29,13 +27,13 @@ exports.emptyDB = function(req,res) {
     db.collection('kaboodleprojects').remove({},function(err,numberRemoved){
         console.log("remove kaboodle projects");
     });
-    db.collection('kaboodletypes').remove({},function(err,numberRemoved){
+    db.collection('kaboodleObjectTypes').remove({},function(err,numberRemoved){
         console.log("remove kaboodle types");
     });
     db.collection('kaboodlelists').remove({},function(err,numberRemoved){
             console.log("remove kaboodle lists");
     });
-        db.collection('kaboodletypes').remove({},function(err,numberRemoved){
+        db.collection('kaboodleObjectTypes').remove({},function(err,numberRemoved){
         console.log("remove kaboodle types");
     });
     db.collection('kaboodletags').remove({},function(err,numberRemoved){
@@ -43,13 +41,13 @@ exports.emptyDB = function(req,res) {
     });
     db.collection('kaboodleobjects', {strict:true}, function(err, collection) {
         populateDB();
-        res.send('Database Reset');
+//        res.send('Database Reset');
     });
 };
 
 var populateDB = function() {
     var fs = require('fs');
-    var kaboodleobjects = './app/controllers/testData/kaboodleobjects.json';
+    var kaboodleobjects = 'kaboodleobjects.json';
 
     fs.readFile(kaboodleobjects, 'utf8', function(err, data) {
         if (err) {
@@ -69,13 +67,13 @@ var populateDB = function() {
         });
     });
 
-    var objecttypes = './app/controllers/testData/kaboodletypes.json';
+    var objecttypes = 'kaboodleObjectTypes.json';
     fs.readFile(objecttypes, 'utf8', function(err, data) {
         if (err) {
             throw err;
         }
         var objects = JSON.parse(data);
-        db.collection('kaboodletypes', function(err, collection) {
+        db.collection('kaboodleObjectTypes', function(err, collection) {
             if (err) {
                 throw err;
             }
@@ -88,7 +86,7 @@ var populateDB = function() {
         });
     });
 
-    var fieldtypes = './app/controllers/testData/kaboodlefieldtypes.json';
+    var fieldtypes = 'kaboodlefieldtypes.json';
     fs.readFile(fieldtypes, 'utf8', function(err, data) {
         if (err) {
             throw err;
@@ -107,7 +105,7 @@ var populateDB = function() {
         });
     });
 
-    var kaboodletags = './app/controllers/testData/kaboodletags.json';
+    var kaboodletags = 'kaboodletags.json';
     fs.readFile(kaboodletags, 'utf8', function(err, data) {
         if (err) {
             throw err;
@@ -126,3 +124,5 @@ var populateDB = function() {
         });
     })
 };
+
+emptyDb();
---
+++ test/testdata/restDB.test.js
@@ -0,0 +1,275 @@
+/**
+ * Created by dibster on 11/19/13.
+ */
+
+var should = require('should');
+var request = require('supertest');
+
+var api = 'http://localhost:3000';
+
+// reset the database here
+
+var recordIdForRead = "";
+request(api)
+    .get('/api/resetDB')
+    .expect(200)
+    .end(function(err, res) {
+        if (err) {
+            throw err;
+        }
+        res.text.should.contain('Database Reset');
+    });
+
+describe('Project Tests', function() {
+
+    before(function(done) {
+
+        var newFaceBookProject = {
+            "Title" : "FaceBook Project",
+            "Description": "Facebook Channel",
+            "Channel Type " : "Facebook",
+            "Type" : "Social Media",
+            "Status" : "Open",
+            "tasks" : [
+                {
+                    "name" : "Receive Brief",
+                    "date" : "2014-01-16T00:00:00.000Z",
+                    "who" : "Test",
+                    "u" : 1,
+                    "cd" : "2014-01-16T08:30:07.441Z",
+                    "taskStatus" : "Open"
+                },
+                {
+                    "name" : "Ingest Content",
+                    "date" : "2014-01-16T00:00:00.000Z",
+                    "who" : "Test",
+                    "u" : 1,
+                    "cd" : "2014-01-16T08:30:26.560Z",
+                    "taskStatus" : "Open"
+                },
+                {
+                    "name" : "Create Quote",
+                    "date" : "2014-01-17T00:00:00.000Z",
+                    "who" : "Test",
+                    "u" : 1,
+                    "cd" : "2014-01-16T08:30:51.238Z",
+                    "taskStatus" : "Open"
+                },
+                {
+                    "name" : "Produce Project Plan",
+                    "date" : "2014-01-17T00:00:00.000Z",
+                    "who" : "Test",
+                    "u" : 1,
+                    "cd" : "2014-01-16T08:31:12.820Z",
+                    "taskStatus" : "Open"
+                }
+            ]
+
+        };
+
+
+        request(api)
+            .post('/api/kaboodleprojects')
+            .send(newFaceBookProject)
+            .expect(200)
+            .end(function(err, res) {
+                if (err) {
+                    throw err;
+                }
+                res.body._id.should.have.length(24);
+                recordIdForRead = res.body._id;
+                done();
+            });
+    });
+
+    describe('Testing FaceBook Project', function() {
+
+        it('should find the record with id ' + recordIdForRead, function(done) {
+            request(api)
+                .get('/api/kaboodleprojects/' + recordIdForRead)
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.body._id.should.have.length(24);
+                    done();
+                });
+        });
+
+        it('should update the record', function(done) {
+            var FaceBookProjectUpdate = {
+                "Title" : "FaceBook Project",
+                "Description": "Facebook Channel Updated",
+                "Channel Type " : "Facebook",
+                "Type" : "Social Media",
+                "Status" : "Open",
+                "tasks" : [
+                    {
+                        "name" : "Receive Brief",
+                        "date" : "2014-01-16T00:00:00.000Z",
+                        "who" : "Test",
+                        "u" : 1,
+                        "cd" : "2014-01-16T08:30:07.441Z",
+                        "taskStatus" : "Open"
+                    },
+                    {
+                        "name" : "Ingest Content",
+                        "date" : "2014-01-16T00:00:00.000Z",
+                        "who" : "Test",
+                        "u" : 1,
+                        "cd" : "2014-01-16T08:30:26.560Z",
+                        "taskStatus" : "Open"
+                    },
+                    {
+                        "name" : "Create Quote",
+                        "date" : "2014-01-17T00:00:00.000Z",
+                        "who" : "Test",
+                        "u" : 1,
+                        "cd" : "2014-01-16T08:30:51.238Z",
+                        "taskStatus" : "Open"
+                    },
+                    {
+                        "name" : "Produce Project Plan",
+                        "date" : "2014-01-17T00:00:00.000Z",
+                        "who" : "Test",
+                        "u" : 1,
+                        "cd" : "2014-01-16T08:31:12.820Z",
+                        "taskStatus" : "Open"
+                    }
+                ],
+                "Invoices" : [
+                    {
+                        "Title" : "Pay This",
+                        "date" : "2014-01-16T00:00:00.000Z",
+                        "who" : "Test",
+                        "u" : 1,
+                        "cd" : "2014-01-16T08:30:07.441Z",
+                        "Status" : "Open"
+                    },
+                    {
+                        "Title" : "Pay This",
+                        "date" : "2014-01-16T00:00:00.000Z",
+                        "who" : "Test",
+                        "u" : 1,
+                        "cd" : "2014-01-16T08:30:07.441Z",
+                        "Status" : "Open"
+                    },
+                    {
+                        "Title" : "Pay This",
+                        "date" : "2014-01-16T00:00:00.000Z",
+                        "who" : "Test",
+                        "u" : 1,
+                        "cd" : "2014-01-16T08:30:07.441Z",
+                        "Status" : "Open"
+                    }
+                ]
+
+            };
+            request(api)
+                .put('/api/kaboodleprojects/' + recordIdForRead)
+                .send(FaceBookProjectUpdate)
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.body.Description.should.be.exactly('Facebook Channel Updated');
+                    done();
+            });
+        });
+
+        it('should return a project for userid  Test', function(done) {
+            request(api)
+                .get('/api/kaboodleprojectsforuser/Test')
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.body[0].should.have.property('Title');
+                    res.body[0].tasks.length.should.eql(4);
+                    done();
+                });
+
+        });
+
+        it('should not return anything for user IsNotDefined', function(done) {
+            request(api)
+                .get('/api/kaboodleprojectsforuser/IsNotDefined')
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.body.should.not.have.property('Title');
+                    done();
+                });
+
+        });
+
+        it('Show the invoices for an Updated Record ' + recordIdForRead, function(done) {
+            request(api)
+                .get('/api/kaboodleprojects/' + recordIdForRead)
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.body.should.have.property('Invoices');
+                    done();
+                });
+        });
+
+        it('should update a task on a project', function(done) {
+
+            var taskCreateDate =  "2014-01-16T08:30:07.441Z";
+
+            var updatedTask = {
+                "name" : "Receive Brief Updated and Closed",
+                "date" : "2014-01-16T00:00:00.000Z",
+                "who" : "Dave",
+                "u" : 1,
+                "cd" : "2014-01-16T08:30:07.441Z",
+                "taskStatus" : "Closed"
+            };
+
+            request(api)
+                .put('/api/kaboodleprojects/' + recordIdForRead + "/tasks/" + taskCreateDate)
+                .send(updatedTask)
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.text.should.contain('Task Updated');
+                    done();
+                });
+        });
+
+
+        it('should do nothing', function(done) {
+            console.log('Done');
+            done();
+        });
+    });
+});
+
+//describe('second describe', function() {
+//    console.log('second describe');
+//
+//    before(function(done) {
+//        console.log('second before has done');
+//        done();
+//    });
+//
+//    describe('test inside test', function() {
+//        it('should find a record and return 200', function(done) {
+//            console.log('secoind test inside second test with done');
+//            done();
+//        });
+//    });
+//
+//});
+//
+
