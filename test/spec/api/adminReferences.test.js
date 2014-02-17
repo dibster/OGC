e24cd9cddac66b00c148a81d5a6155b8936e7213
Dave Ashenhurst <dave.ashenhurst@gmail.com>
2014-02-17 12:13:14 +0000

    Tests For Lookus Done

---
+++ test/spec/api/adminReferences.test.js
@@ -0,0 +1,41 @@
+/**
+ * Created by dibster on 11/19/13.
+ */
+
+var should = require('should');
+var request = require('supertest');
+
+var api = 'http://localhost:9000';
+
+describe(' Admin Tests', function() {
+
+    describe('Lookup Tests', function() {
+
+        it('Return 11 field types', function(done) {
+            request(api)
+                .get('/api/admin/fieldtypes/')
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.body.should.have.length(11);
+                    done();
+                });
+        });
+
+        it('Return 5 object types', function(done) {
+            request(api)
+                .get('/api/admin/objecttypes/')
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    res.body.should.have.length(5);
+                    done();
+                });
+        });
+    });
+});
+
