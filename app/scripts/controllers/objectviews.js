fb74d6b32c364ed0a755b44bd5cc11d0909334b4
Dave Ashenhurst <dave.ashenhurst@gmail.com>
2014-03-04 22:14:34 +0000

    add fields

--- app/scripts/controllers/objectviews.js
+++ app/scripts/controllers/objectviews.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('ogcApp')
-    .controller('ObjectViewsCtrl', function ($scope, $routeParams, $modal, $log,  Objects) {
+    .controller('ObjectViewsCtrl', function ($scope, $routeParams, Objects) {
 
         $scope.object = {};
 
@@ -9,5 +9,29 @@ angular.module('ogcApp')
             $scope.object = object;
           });
 
-      });
+        $scope.addFieldToViews = function(selectedItem) {
 
+            $scope.object._id = $routeParams.id;
+            // iterate through all views
+            var numberOfViews = $scope.object.views.length;
+            for(var i=0;i<numberOfViews;i++) {
+              var fieldExists = false;
+              // get the array length
+              var arrayLength = $scope.object.views[i].fields.length;
+              // does the field exist
+              for (var index = 0; index < arrayLength; ++index) {
+                if ($scope.object.views[i].fields[index].name === selectedItem.name) {
+                  fieldExists = true;
+                  break;
+                }
+              }
+              if (!fieldExists) {
+                $scope.object.views[i].fields.push(selectedItem);
+              }
+            }
+
+            $scope.object.$update(function(response) {
+              $scope.object = response;
+            });
+          };
+      });
