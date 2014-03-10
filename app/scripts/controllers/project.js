f02018cbed445fe4c25b0cc9c56e168ff2013a3d
Dave Ashenhurst <dave.ashenhurst@gmail.com>
2014-03-10 07:53:03 +0000

    before modal test, has hashtags defined

--- app/scripts/controllers/project.js
+++ app/scripts/controllers/project.js
@@ -1,27 +1,76 @@
 'use strict';
 
 angular.module('ogcApp')
-    .controller('ProjectCtrl', function ($scope, Objects, ObjectTypes, Projects) {
+    .controller('ProjectCtrl', function ($scope, Objects, ObjectTypes, PrepareRecord, Projects, hashTags) {
 
-        console.log('in controller');
+        $scope.projectInstances = [{}];
+
         ObjectTypes.query(function(response) {
             $scope.projectTypes = response;
           });
 
-//        Projects.query(function(response) {
-//            $scope.projects = response;
-//          });
-//
-
-
-
-
         // project tree build.
+        //        $scope.myData = [{
+        //            label: 'Languages',
+        //            children: ['Jade','Less','CoffeeScript']
+        //          }];
 
-//        $scope.myData = [{
-//            label: 'Languages',
-//            children: ['Jade','Less','CoffeeScript']
-//          }];
+        $scope.saveFormDetails = function(formData) {
+            var myRecord = PrepareRecord.getRecord(formData,$scope.selectedType);
+            var ogcProject = new Projects(myRecord);
 
+            $scope.project = ogcProject.$save(function(response) {
+                $scope.projectInstances.push(response);
+                //                return response;
+              });
+          };
+
+        $scope.openCreateProjectModal = function (selectedObject) {
+            // get tags
+            $scope.selectedType = selectedObject.name;
+            var modalInstance = $modal.open({
+                templateUrl: 'partials/modalProjectCreate.html',
+                controller: ModalInstanceCtrl,
+                resolve: {
+                    items: function () {
+                        return selectedObject.views[0].fields;
+                      }
+                  }
+                });
+
+            modalInstance.result.then(function (selectedItem) {
+                $scope.formData = selectedItem;
+                $scope.saveFormDetails($scope.formData);
+
+              }, function () {
+                console.log('Modal dismissed at: ' + new Date());
+              });
+          };
+
+
+        var ModalInstanceCtrl = function($scope,$modalInstance, items) {
+
+            $scope.items = items;
+            $scope.bannertypes = ['40K Banner', 'Static Banner', '100K Banner'];
+
+            // this would get values from backend,  but not implemented yet
+
+            $scope.getTagValues = function(fieldname) {
+                //
+                hashTags.get({id : fieldname},function(tag) {
+                    //$log.info('tag values : ' + tag.values[0].value );
+                    return tag.values;
+                  });
+              };
+
+            $scope.ok = function () {
+                $modalInstance.close($scope.items);
+              };
+
+            $scope.cancel = function () {
+                $modalInstance.dismiss('cancel');
+              };
+
+          };
       });
 
--- app/scripts/services/ogcService.js
+++ app/scripts/services/ogcService.js
@@ -16,8 +16,23 @@ angular.module('ogcApp')
         return $resource(url + 'api/projects/:id', {id:'@_id'}, {update:{method: 'PUT'}});
       })
 
-
     .factory('ObjectFieldTypes', function($resource){
         return $resource(url + 'api/admin/fieldtypes', {});
-      });
+      })
 
+    .factory('hashTags', function($resource){
+        return $resource(url + 'api/hashtags', {});
+      })
+
+    .service('PrepareRecord', function() {
+        this.getRecord = function(formData, type) {
+            var jsonRecord = {'Type' : type};
+            formData.forEach(function(field) {
+                if (field.content !== null) {
+                  var fieldname = field.name;
+                  jsonRecord[fieldname] = field.content;
+                }
+              });
+            return jsonRecord;
+          };
+      });
