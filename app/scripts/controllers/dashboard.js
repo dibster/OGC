71b6734b6f684110b72f8d083d5eabb10ad49bea
Dave Ashenhurst <dave.ashenhurst@gmail.com>
2014-03-14 13:44:16 +0000

    Start Dashboard

---
+++ app/scripts/controllers/dashboard.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('ogcApp')
+    .controller('ProjectDashboardCtrl', function ($scope, Objects, ObjectTypes, ProjectTypes, PrepareRecord, $modal, Projects, hashTags, ngTableParams) {
+
+
+});
+
