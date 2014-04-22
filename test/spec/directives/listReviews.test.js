c0dc119f40bc03a39d4671ae7119e3fd7ea6bb6f
Dave Ashenhurst <dave.ashenhurst@gmail.com>
2014-04-22 14:27:44 +0100

    Adding Review Directive Test Spec

---
+++ test/spec/directives/listReviews.test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+describe('Directive: fileList', function () {
+
+    // load the directive's module
+    beforeEach(module('ogcApp'));
+    // load the html
+    beforeEach(module('scripts/directives/templates/listReviews.html'));
+    var element,
+        scope;
+
+    beforeEach(inject(function ($rootScope, $compile) {
+        element = angular.element('<div list-reviews reviews="reviews"></div>');
+        scope = $rootScope.$new();
+
+        var testReviews = [{}];
+        testReviews[0].request = 'This is a review request';
+        testReviews[0].decision = '';
+        testReviews[0].date = new Date();
+        // compile
+        //
+        //
+        // element
+        var e = $compile(element)(scope);
+        // set the isolated Scope value
+        e.scope().reviews = testReviews;
+        e.scope().$digest();
+
+      }));
+
+    it('should show a review item', inject(function () {
+        expect(element.text()).toContain('This is a review');
+      }));
+  });
+/**
+ * Created by dibster on 14/03/14.
+ */
